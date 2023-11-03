540370b5660e67d693918b04b8c3da622af7c347
Gabriel Penke <gabri.penke@gmail.com>
2023-11-02 22:40:25 -0300

    feat: usign the context on Guest layout

--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useStateContext } from '../views/ContextProvider'
 
 // Outlet is the place where the child will be rendered
 const GuestLayout = () => {
+  const {token} = useStateContext();
+
+  if(token){
+    return <Navigate to="/users" />
+  }
   return (
     <div>
         <Outlet />
