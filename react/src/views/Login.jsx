a046acce173fca27d3d8e8ca09700a8cd3b21539
Gabriel Penke <gabri.penke@gmail.com>
2023-11-20 22:28:48 -0300

    feat: marcação do form de login

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,3 +1,19 @@
 export default () => {
-    return <>Login</>;
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+    return (
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form action="" onSubmit={onSubmit}>
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <button className="btn btn-block">Login</button>
+                    <p className="message">
+                        Not Registered? <a href="/signup">Create an account</a>
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
 };
