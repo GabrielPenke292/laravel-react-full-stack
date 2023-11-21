34e544fb9310ebc6cd13901a47836e153db52403
Gabriel Penke <gabri.penke@gmail.com>
2023-11-20 22:14:25 -0300

    refactor: redireciona para dash usuarios logado

--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -7,7 +7,7 @@ const GuestLayout = () => {
     const { token } = useStateContext();
 
     if (token) {
-        return <Navigate to="/users" />;
+        return <Navigate to="/dashboard" />;
     }
     return (
         <div>
