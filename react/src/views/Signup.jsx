1cd1c64a574644f285d8c488138feb69fddc7188
Gabriel Penke <gabri.penke@gmail.com>
2023-11-25 18:44:19 -0300

    fix: erro de sintaxe

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import axiosClient from "../axios-client";
 import { useStateContext } from "../contexts/ContextProvider";
 
@@ -7,6 +7,7 @@ const Signup = () => {
     const emailRef = useRef();
     const passwordRef = useRef();
     const passwordConfirmationRef = useRef();
+    const [errors, setErrors] = useState(null);
 
     const { setUser, setToken } = useStateContext();
 
@@ -27,9 +28,8 @@ const Signup = () => {
             })
             .catch((err) => {
                 const response = err.response;
-                console.log(err);
                 if (response && response.status === 422) {
-                    console.log(response.data.errors);
+                    setErrors(response.data.errors);
                 }
             });
     };
@@ -37,6 +37,13 @@ const Signup = () => {
     return (
         <form action="" onSubmit={onSubmit}>
             <h1 className="title">Sign up for free</h1>
+            {errors && (
+                <div className="alert alert-danger">
+                    {Object.keys(errors).map((key) => (
+                        <p key={key}>{errors[key][0]}</p>
+                    ))}
+                </div>
+            )}
             <input ref={nameRef} type="text" placeholder="Full Name" />
             <input ref={emailRef} type="email" placeholder="Email Address" />
             <input ref={passwordRef} type="password" placeholder="Password" />
