9f62771b440ffa5ffedb96be4cd9542eea4459b8
Gabriel Penke <gabri.penke@gmail.com>
2023-12-02 23:19:42 -0300

    feat: lógica de login

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,12 +1,45 @@
+import React, { useRef, useState } from "react";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
+
 export default () => {
+    const passwordRef = useRef();
+    const emailRef = useRef();
+    const [errors, setErrors] = useState(null);
+    const { setUser, setToken } = useStateContext();
+
     const onSubmit = (e) => {
         e.preventDefault();
+        const payload = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        };
+
+        axiosClient
+            .post("/login", payload)
+            .then(({ data }) => {
+                setToken(data.token);
+                setUser(data.user);
+            })
+            .catch((err) => {
+                const response = err.response;
+                if (response && response.status === 422) {
+                    setErrors(response.data.errors);
+                }
+            });
     };
     return (
         <form action="" onSubmit={onSubmit}>
             <h1 className="title">Login into your accout</h1>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            {errors && (
+                <div className="alert alert-danger">
+                    {Object.keys(errors).map((key) => (
+                        <p key={key}>{errors[key][0]}</p>
+                    ))}
+                </div>
+            )}
+            <input ref={emailRef} type="email" placeholder="Email" />
+            <input ref={passwordRef} type="password" placeholder="Password" />
             <button className="btn btn-block">Login</button>
             <p className="message">
                 Not Registered? <a href="/signup">Create an account</a>
